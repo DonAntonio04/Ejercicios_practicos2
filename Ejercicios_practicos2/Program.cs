cc19401e0cb4b7da07a4b9368cf962912396f86f
hp <hp@DESKTOP-HADGCVI>
2023-11-02 12:57:04 -0700

    Agregar archivos de proyecto.

---
+++ Ejercicios_practicos2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Ejercicios_practicos2;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Practicos practicos = new Practicos();
+
+        Console.WriteLine("Ejercicio 1");
+        practicos.ejercicio1();
+
+        //Console.WriteLine("Ejercicio 2");
+        //practicos .ejercicio2();
+        //Console.WriteLine("Ejercicio 3");
+        //practicos.ejercicio3 ();
+        //Console.WriteLine("Ejercicio 4");
+        //practicos.ejercicio4 ();
+        //Console.WriteLine("Ejercicio 5");
+        //practicos.ejercicio5 ();
+    }
+}
